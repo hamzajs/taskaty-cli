f3bfb7919e45e3d84d9b6bb71cc44f088237f55f
hamzajs <hamza.sulieman22@gmail.com>
2026-04-12 20:16:29 +0300

    feat: add `list_tasks` and `list_all_tasks` method

--- taskaty/TaskController.py
+++ taskaty/TaskController.py
@@ -18,3 +18,32 @@ class TaskController:
         with open(self.file_name, 'a') as file:
             file.write(str(task) + '\n')
         print(f"Task '{task.title}' added successfully!")
+
+    def list_tasks(self):
+        unfinished_tasks = []
+        with open(self.file_name, 'r') as file:
+            for line in file:
+                title, description, start_date, end_date, done = line.split(', ')
+
+                end_date = None if end_date == 'None' else end_date
+                done = False if done.strip('\n') == 'False' else True
+                
+                if done:
+                    continue
+                unfinished_tasks.append({'title': title, 'description': description, 
+                                        'start_date': start_date, 'end_date': end_date})
+        return unfinished_tasks
+    
+    def list_all_tasks(self):
+        all_tasks = []
+        with open(self.file_name, 'r') as file:
+            for line in file:
+                title, description, start_date, end_date, done = line.split(', ')
+
+                end_date = None if end_date == 'None' else end_date
+                done = False if done.strip('\n') == 'False' else True
+
+                all_tasks.append({'title': title, 'description': description, 
+                                  'start_date': start_date, 'end_date': end_date,
+                                  'done': done})
+        return all_tasks
