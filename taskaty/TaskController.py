500e04fb4cd933beda7fe44839e1ee861f394169
hamzajs <hamza.sulieman22@gmail.com>
2026-04-12 20:05:26 +0300

    feat: add add_task method

--- taskaty/TaskController.py
+++ taskaty/TaskController.py
@@ -6,3 +6,15 @@ class TaskController:
 
     def __init__(self, file_name):
         self.file_name = file_name
+
+    def add_task(self, args):
+        if not args.start_date:
+            now = date.today().isoformat()
+            args.start_date = now
+
+        task = Task(args.title, args.description, args.start_date, args.end_date, 
+                    args.done)
+
+        with open(self.file_name, 'a') as file:
+            file.write(str(task) + '\n')
+        print(f"Task '{task.title}' added successfully!")
